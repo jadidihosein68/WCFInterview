2722c595fdecb5cec788a075e62233a59b074607
Hossein <jadidi.hosein@gmail.com>
2018-05-01 23:53:36 +0800

    Use Service In MVC !

---
+++ SEBWarmup/Controllers/API/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SEBWarmup.MulServiceReference;
+
+namespace SEBWarmup.Controllers.API
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult get(int a , int b)
+        {
+
+            MulServiceClient MSC = new MulServiceClient("BasicHttpBinding_IMulService");
+
+            var result = MSC.Mul(a,b);
+
+
+            return Ok($"Can ! {result.ToString()}");
+
+        }
+
+    }
+}
