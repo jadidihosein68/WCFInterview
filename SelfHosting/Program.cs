d3a825525984cf77c31636a8c8c3989b8abb9e78
Hossein <jadidi.hosein@gmail.com>
2018-05-06 15:12:11 +0800

    add Mex

--- SelfHosting/Program.cs
+++ SelfHosting/Program.cs
@@ -11,28 +11,9 @@ namespace SelfHosting
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void multiple()
         {
-
-
-            /*
-            //  for single bidning 
-            Uri TCPbaseAddress = new Uri("net.tcp://localhost:9998"); // create base address 
-            ServiceHost sh = new ServiceHost(typeof(MulService), HTTPbaseAddress); // service object  for single bidning 
-            ServiceEndpoint se = sh.AddServiceEndpoint(typeof(IMulService), new WSHttpBinding(), baseAddress);
-            
-            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-            smb.HttpGetEnabled = true;
-            sh.Description.Behaviors.Add(smb); // add behaviod to the sh
-            sh.Open();
-              Console.WriteLine("started ...");
-            Console.WriteLine(se.Address);
-            Console.WriteLine(se.Binding.Name);
-            Console.WriteLine(se.Contract.ContractType);
-            Console.ReadLine();
-            sh.Close();
-             */
-
 
             // for multiple binding 
             Uri TCPbaseAddress = new Uri("net.tcp://localhost:9998"); // create base address 
@@ -50,16 +31,81 @@ namespace SelfHosting
             smb.HttpGetEnabled = true;
             sh.Description.Behaviors.Add(smb); // add behaviod to the sh
             sh.Open();
+            getresult(sh);
+            sh.Close();
+
+        }
+
+
+
+
+
+
+        public static void multipleMEx()
+        {
+
+            // for multiple binding 
+            Uri TCPbaseAddress = new Uri("net.tcp://localhost:9998"); // create base address 
+            Uri HTTPbaseAddress = new Uri("http://localhost:9999"); // create base address 
+            ServiceHost sh = new ServiceHost(typeof(MulService), new Uri[] { TCPbaseAddress, HTTPbaseAddress }); // service object 
+
+            ServiceEndpoint TCPServiceEndpoint = sh.AddServiceEndpoint(typeof(IMulService), new NetTcpBinding(), TCPbaseAddress);
+            ServiceEndpoint HTTPServiceEndpoint = sh.AddServiceEndpoint(typeof(IMulService), new BasicHttpBinding(), HTTPbaseAddress);
+
+
+
+            //ServiceEndpoint se = sh.AddServiceEndpoint(typeof(IMulService), new WSHttpBinding(),baseAddress);
+            // (contract, binding, Address )
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            sh.Description.Behaviors.Add(smb); // add behaviod to the sh
+            sh.Open();
+            getresult(sh);
+            sh.Close();
+
+        }
+
+
+        public static void single (){
+            Uri TCPbaseAddress = new Uri("net.tcp://localhost:9998"); // create base address 
+            ServiceHost sh = new ServiceHost(typeof(MulService), TCPbaseAddress); // service object  for single bidning 
+            ServiceEndpoint se = sh.AddServiceEndpoint(typeof(IMulService), new WSHttpBinding(), TCPbaseAddress);
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            sh.Description.Behaviors.Add(smb); // add behaviod to the sh
+            sh.Open();
+            getresult(sh);
+            sh.Close();
+        }
+
+
+
+
+
+        public static void getresult(ServiceHost sh)
+        {
+
             foreach (var item in sh.Description.Endpoints)
             {
+                Console.WriteLine("started ...");
                 Console.WriteLine($"Address : {item.Address.ToString()}");
                 Console.WriteLine($"Binding : {item.Binding.Name.ToString()}");
                 Console.WriteLine($"Contract : {item.Contract.ContractType.ToString()}");
 
             }
-
             Console.ReadLine();
-            sh.Close();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //single();
+            multiple();
+
+
+
         }
     }
 }
