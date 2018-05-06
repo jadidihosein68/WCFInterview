a6decaaf875524601b4dc0f9b9762ff70a84170e
Hossein <jadidi.hosein@gmail.com>
2018-05-06 17:30:27 +0800

    Add fault service behavior = true

--- MulService/MulService.cs
+++ MulService/MulService.cs
@@ -7,6 +7,8 @@ using System.Text;
 
 namespace MulServiceLibrary
 {
+
+    [ServiceBehavior(IncludeExceptionDetailInFaults =true)]
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class MulService : IMulService
     {
